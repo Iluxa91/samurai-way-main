5c9e4719c126a781dc3f07280dda65efc7371e5c
Ilua Kisialiou <iluxa91@inbox.ru>
2022-08-29 15:59:34 +0300

    login add free account

--- src/components/Login/LoginFormik.tsx
+++ src/components/Login/LoginFormik.tsx
@@ -56,6 +56,16 @@ export const LoginFormik = (props: PropsType) => {
     return (
         <form onSubmit={formik.handleSubmit}>
             <div>
+                <p>To log in get registered
+                    <a href={"https://social-network.samuraijs.com/"}
+                       target={"_blank"} rel="noreferrer"> here
+                    </a>
+                </p>
+                <p>or use common test account credentials:</p>
+                <p>Email: free@samuraijs.com</p>
+                <p>Password: free</p>
+            </div>
+            <div>
                 <input
                     className={formik.errors.email ? s.error : ""}
                     placeholder={"email"}
