d92d246e2fa4e7246d1250656217e838ab4006e7
Ilua Kisialiou <iluxa91@inbox.ru>
2022-09-22 19:10:04 +0300

    radio added

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -1,9 +1,35 @@
-import d from './Music.module.css'
+import React, { useState } from 'react';
+import styles from './Music.module.css'
 
-export const Music = () => {
+
+export function Music() {
+
+    const [switchOnOff, setSwitchOnOff] = useState(false)
+
+    let url = "http://streaming.tdiradio.com:8000/house.mp3";
+
+    let audio = new Audio(url);
+
+    const handleOnOffClick = () => {
+        if (switchOnOff === false) {
+            setSwitchOnOff(true)
+            audio.pause()
+        } else {
+            setSwitchOnOff(false)
+            audio.play()
+        }
+    }
+
     return (
-        <div>
-            Music
+        <div className={styles.pleer}>
+            <div>
+                <div className={styles.item}>Radio📻</div>
+            </div>
+            <button onClick={handleOnOffClick} >
+                {
+                    switchOnOff ? <h2>⏸️</h2> : <h2>▶️</h2>
+                }
+            </button>
         </div>
     )
 }
